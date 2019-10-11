85a71d5b47f29e912c849452926e9c01f480eb7f
sandrobraga <sandrobraga@softbox.com.br>
2019-10-11 14:10:31 -0300

    Page objects

--- tests/index.js
+++ tests/index.js
@@ -1,29 +1,21 @@
+const facebookUser = require('../userPO/FacebookUser');
+const facebookPO   = require('../page_objects/facebookHome');
+
 module.exports = {
-  //andressagurgel55@hotmail.com mail
-  //andressinhagugu89 password
   'Crawler step one' : function (browser) {
     browser
-      .url("https://fb.me")
+      .url(facebookPO.URL)
       .pause(2000)
-      .setValue('input[id="email"]', 'andressagurgel55@hotmail.com')
+      .setValue(facebookPO.EMAIL_INPUT, facebookUser.EMAIL)
       .pause(2000)
-      .setValue('input[id="pass"]', 'andressinhagugu89')
+      .setValue(facebookPO.PASS_INPUT, facebookUser.PASSWORD)
       .pause(2000)
-      .click('label[id="loginbutton"]')
+      .click(facebookPO.LOGIN_BUTTON)
       .pause(10000)
-      .setValue('input[name="q"]', 'Fernando Reiz')
+      .setValue(facebookPO.SEARCH_INPUT, 'Fernando Reiz')
       .pause(2000)
       .keys(browser.Keys.ENTER)
       .pause(200000)
       .end();
-  /*browser
-      .url(browser.options.desiredCapabilities.launchUrl)
-      .pause(2000)
-      .waitForElementVisible('input[id="search"]', 5000, false)
-      .setValue('input[id="search"]', 'Grindcore')
-      .pause(1000)
-      .click('button[id="search-icon-legacy"]')
-      .pause(4000)
-      .end(); */
   }
 };
---
+++ userPO/FacebookUser.js
@@ -0,0 +1,7 @@
+const EMAIL    = "andressagurgel55@hotmail.com";
+const PASSWORD = "andressinhagugu89";
+
+module.exports = {
+  EMAIL,
+  PASSWORD
+}
